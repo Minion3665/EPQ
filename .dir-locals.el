c354b91c44f5e473ddc3685f17f19d0ca63dc608
Skyler Turner <skyler@clicksminuteper.net>
2021-10-18 10:57:22 +0100

    ../../School/EPQ/Documents/personal-audit.org

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil . (
+	 (org-roam-directory . ((concat default-directory "Notes")))
+	 (org-roam-db-location . ((concat default-directory "Notes/org-roam.db")))
+	 (eval git-auto-commit-mode 1)
+	 )))
