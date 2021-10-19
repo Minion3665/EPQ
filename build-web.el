4ae2179f2860ec1f7e08fa6c718f70528c24280e
Skyler Turner <skyler@clicksminuteper.net>
2021-10-19 23:19:55 +0100

    ../../School/EPQ/build-web.el

--- build-web.el
+++ build-web.el
@@ -58,7 +58,7 @@ targets and targets."
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
       org-html-head
-      "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+      "<link rel=\"stylesheet\" href=\"/simple.min.css\" /><link rel=\"stylesheet\" href=\"/custom.css\" />")
 
 (setq org-publish-project-alist
       (list
