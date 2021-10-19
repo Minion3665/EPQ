61a4cb340ea1f197b7029629a0d646feca2efd48
Skyler Turner <skyler@clicksminuteper.net>
2021-10-19 20:38:50 +0100

    ../../School/EPQ/build-web.el

--- build-web.el
+++ build-web.el
@@ -14,6 +14,46 @@
 (package-install 'org)
 (package-install 'org-roam)
 
+(require 'org-roam)
+
+(setq org-roam-directory (file-truename "./content"))
+(setq org-id-link-to-org-use-id t)
+;; (org-roam-db-autosync-mode)
+
+(setq org-id-extra-files (org-roam-list-files))
+
+(defun org-html--reference (datum info &optional named-only)
+  "Return an appropriate reference for DATUM.
+DATUM is an element or a `target' type object.  INFO is the
+current export state, as a plist.
+When NAMED-ONLY is non-nil and DATUM has no NAME keyword, return
+nil.  This doesn't apply to headlines, inline tasks, radio
+targets and targets."
+  (let* ((type (org-element-type datum))
+	 (user-label
+	  (org-element-property
+	   (pcase type
+	     ((or `headline `inlinetask) :CUSTOM_ID)
+	     ((or `radio-target `target) :value)
+	     (_ :name))
+	   datum))
+         (user-label (or user-label
+                         (when-let ((path (org-element-property :ID datum)))
+                           (concat "ID-" path)))))
+    (cond
+     ((and user-label
+	   (or (plist-get info :html-prefer-user-labels)
+	       ;; Used CUSTOM_ID property unconditionally.
+	       (memq type '(headline inlinetask))))
+      user-label)
+     ((and named-only
+	   (not (memq type '(headline inlinetask radio-target target)))
+	   (not user-label))
+      nil)
+     (t
+      (org-export-get-reference datum info)))))
+
+
 (setq org-html-validation-link nil
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
