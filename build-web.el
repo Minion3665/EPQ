82605021f0b1ce627c23902799dc228b5917fb01
Skyler Turner <skyler@clicksminuteper.net>
2021-10-19 20:14:22 +0100

    ../../School/EPQ/build-web.el

--- build-web.el
+++ build-web.el
@@ -1,3 +1,13 @@
 (require 'ox-publish)
 
+(setq org-publish-project-alist
+      (list
+       (list "epq-project-docs"
+	     :recursive t
+	     :base-directory "./Web"
+	     :publishing-directory "./Web-out"
+	     :publishing-function 'org-html-publish-to-html)))
+
+(org-publish-all t)
+
 (message "Built website")
