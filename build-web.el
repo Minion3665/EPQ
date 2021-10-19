496cd407d26bcbbcdaadf837290d7e59f45b3028
Skyler Turner <skyler@clicksminuteper.net>
2021-10-19 22:51:49 +0100

    ../../School/EPQ/build-web.el

--- build-web.el
+++ build-web.el
@@ -77,7 +77,7 @@ targets and targets."
 	     )
        (list "org-static"
 	     :base-directory "./Web"
-	     :base-extension ".*"
+	     :base-extension "^(?!org$).*"
 	     :publishing-directory "./Web-out"
 	     :recursive t
 	     :publishing-function 'org-publish-attachment
