ae31c7ce2b915cfae438793b76384b058ff02f27
Skyler Turner <skyler@clicksminuteper.net>
2021-10-19 23:59:27 +0100

    ../../School/EPQ/build-web.el

--- build-web.el
+++ build-web.el
@@ -58,7 +58,8 @@ targets and targets."
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
       org-html-head
-      "<link rel=\"stylesheet\" href=\"/simple.min.css\" /><link rel=\"stylesheet\" href=\"/custom.css\" />")
+      "<link rel=\"stylesheet\" href=\"/simple.min.css\" /><link rel=\"stylesheet\" href=\"/custom.css\" />"
+      org-html-footer "<a href=\"https://github.com/Minion3665/EPQ\">github</a>")
 
 (setq org-publish-project-alist
       (list
