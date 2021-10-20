3af35456b66c74684982c5d67d1a5d553242df94
Skyler Turner <skyler@clicksminuteper.net>
2021-10-20 10:07:15 +0100

    ../../School/EPQ/build-web.el

--- build-web.el
+++ build-web.el
@@ -1,8 +1,8 @@
 (require 'ox-publish)
 (require 'package)
 (setq package-user-dir (expand-file-name "./.packages"))
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")))
+;                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
 ;; Initialize the package system
 (package-initialize)
