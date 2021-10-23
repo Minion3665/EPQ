4e7322a2a77c44e269d50b9467146d49f16b336d
Skyler Turner <skyler@clicksminuteper.net>
2021-10-23 22:53:55 +0100

    build-web.el

--- build-web.el
+++ build-web.el
@@ -63,7 +63,7 @@ targets and targets."
 (defun org-publish-org-sitemap (title list)
   "Sitemap generation function."
   (concat "#+TITLE: Sitemap\n\n"
-          (org-list-to-generic list)))
+          (org-list-to-generic list :ustart "-" :nobr true)))
 
 (setq org-publish-project-alist
       (list
