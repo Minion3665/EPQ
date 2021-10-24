8e19c198095f985e03668968f3d63d89c60b6a51
Skyler Turner <skyler@clicksminuteper.net>
2021-10-24 20:58:23 +0100

    ../../School/EPQ/build-web.el

--- build-web.el
+++ build-web.el
@@ -85,7 +85,7 @@ For my EPQ, I'm making a process management daemon. I [[file:Documents/process-m
   (let ((filename (org-publish-find-title entry project)))
     (if (= (length filename) 0)
         (format "*%s*" entry)
-      (format "[[file:%s][%s]] (Created at %s)"
+      (format "[[file:%s][%s]] (Created %s)"
               entry
               filename
 	      (format-time-string "on %Y-%m-%d at %H:%M:%S"
