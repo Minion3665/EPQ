1d2a0ece981e3463dcfabe3f198a112677dcbb0d
Skyler Turner <skyler@clicksminuteper.net>
2021-10-27 16:47:02 +0100

    ../../School/EPQ/.dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,7 +2,7 @@
 	 (org-roam-directory . "~/School/EPQ/Notes")
 	 (org-roam-db-location . "~/School/EPQ/Notes/org-roam.db")
 	 (eval git-auto-commit-mode 1)
-	 (org-roam-dailies-capture-templates '(("d" "default" plain "%?"
+	 (org-roam-dailies-capture-templates `(("d" "default" plain "%?"
 						:target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
 								   "#+title: ${title}\n")
 						:unnarrowed t)
