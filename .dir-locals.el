60677c270636a94dd7099ffb42de1486f8575b9d
Skyler Turner <skyler@clicksminuteper.net>
2021-10-27 16:29:40 +0100

    ../../School/EPQ/.dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,12 +3,13 @@
 	 (org-roam-db-location . "~/School/EPQ/Notes/org-roam.db")
 	 (eval git-auto-commit-mode 1)
 	 (org-roam-capture-templates . '(
-					 ("n" "default" plain "%?"
-					  :target (file+head "${slug}.org"
-							     "#+title: ${title}\n")
-					  :unnarrowed t)
-					 ("d" "diary" plain "%?"
-					  :target (file+head "${slug}.diary.org"
-							     "#+title: Diary entry for ${title}\n"))
+					 (("d" "default" plain "%?"
+					   :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+							      "#+title: ${title}\n")
+					   :unnarrowed t))
+					 (("t" "diary" plain "%?"
+					   :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+							      "#+title: Diary entry for ${title}\n* What have I done since the last entry?\n* What have I learned & how?\n* What worked well?\n* What would I do differently next time?\n")
+					   :unnarrowed t))
 					 ))
 	 )))
