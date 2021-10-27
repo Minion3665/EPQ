5901a0f639ea39aff39dcb696d401bbe0df2770c
Skyler Turner <skyler@clicksminuteper.net>
2021-10-27 18:56:41 +0100

    ../../School/EPQ/.dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,7 +6,6 @@
 					      (file+head "%<%Y-%m-%d>.org" "#+title: ${title}\n")
 					      :unnarrowed t)
 					     ("d" "diary" entry "* Diary entry%?\n** What have I done since the last entry?\n** What have I learned & how?\n** What worked well?\n** What would I do differently next time?\n" :if-new
-					      (file+head "%<%Y-%m-%d>.org" "#+title: ${title}\n* Diary entry\n** What have I done since the last entry?\n** What have I learned & how?\n** What worked well?\n** What would I do differently next time?")
-					      :unnarrowed nil)
+					      (file+head "%<%Y-%m-%d>.org" "#+title: ${title}\n* Diary entry\n** What have I done since the last entry?\n** What have I learned & how?\n** What worked well?\n** What would I do differently next time?"))
 					     )
 	 )))
