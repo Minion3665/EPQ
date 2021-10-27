79f355a1804258895c925b8e3082bd3fc944e80e
Skyler Turner <skyler@clicksminuteper.net>
2021-10-27 18:10:08 +0100

    ../../School/EPQ/.dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,7 +5,7 @@
 	 (org-roam-dailies-capture-templates ("n" "default" entry "* %?" :if-new
 					      (file+head "%<%Y-%m-%d>.org" "#+title: ${title}\n")
 					      :unnarrowed t)
-					     ("d" "diary" entry "* Diary entry\n** What have I done since the last entry?\n** What have I learned & how?\n** What worked well?\n** What would I do differently next time?\n" :if-new
+					     ("d" "diary" entry "* Diary entry%?\n** What have I done since the last entry?\n** What have I learned & how?\n** What worked well?\n** What would I do differently next time?\n" :if-new
 					      (file+head "%<%Y-%m-%d>.org" "#+title: ${title}\n* Diary entry\n** What have I done since the last entry?\n** What have I learned & how?\n** What worked well?\n** What would I do differently next time?")
 					      :unnarrowed t)
 					     )
