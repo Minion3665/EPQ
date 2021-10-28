375cb9e482ef014c673cd3067c1d727928dee765
Skyler <skyler3665@gmail.com>
2021-10-28 22:35:18 +0100

    Update build-web.el

--- build-web.el
+++ build-web.el
@@ -97,7 +97,7 @@ For my EPQ, I'm making a process management daemon. I [[file:Documents/process-m
   '(lambda (x) (not (nth 1 x))) ; non-directory
   (sort
    (directory-files-and-attributes path nil ".*\.org$" t)
-   '(lambda (x y) (time-less-p (nth 5 y) (nth 5 x)))))) ; last modified first: y < x
+   '(lambda (x y) (time-less-p (nth 5 y) (nth 5 x))))))) ; last modified first: y < x
 ;; https://stackoverflow.com/questions/30886282/emacs-lisp-how-can-i-get-the-newest-file-in-a-directory
 
 (setq org-publish-project-alist
