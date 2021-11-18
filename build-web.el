23a0f2eb0eaab571ec62ef78c425cb504454793a
Skyler Turner <skyler@clicksminuteper.net>
2021-11-18 11:57:31 +0000

    ../../School/EPQ/build-web.el

--- build-web.el
+++ build-web.el
@@ -1,7 +1,8 @@
 (require 'package)
 (setq package-user-dir (expand-file-name "./.packages"))
 (setq package-archives '(("nongnu" . "https://elpa.nongnu.org/nongnu/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+			 ("elpa" . "https://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")))
 
 ;; Initialize the package system
 (package-initialize)
