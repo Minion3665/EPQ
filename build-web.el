2378db1723fab05be5bc819fa7244ee4b4ac3801
Skyler Turner <skyler@clicksminuteper.net>
2021-11-21 13:46:50 +0000

    ../../School/EPQ/build-web.el

--- build-web.el
+++ build-web.el
@@ -23,7 +23,8 @@
 (setq org-roam-directory (file-truename "./Notes"))
 (setq org-cite-global-bibliography (list (file-truename "./Documents/bibliography.bib")))
 (setq org-id-link-to-org-use-id t)
-(setq org-cite-export-processors '((t csl . ((file-truename "./harvard-anglia-ruskin-university.csl")))))
+(setq skyler/csl-path (file-truename "./harvard-anglia-ruskin-university.csl"))
+(setq org-cite-export-processors '((t csl (skyler/csl-path))))
 ;; (org-roam-db-autosync-mode)
 
 (setq org-id-extra-files (org-roam-list-files))
