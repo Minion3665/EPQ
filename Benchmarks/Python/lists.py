7dd43af9d5c05b5ca2856db5ccb0f0a4e558461e
Skyler Turner <skyler@clicksminuteper.net>
2021-11-29 11:10:52 +0000

    ../../School/EPQ/Benchmarks/Python/lists.py

--- Benchmarks/Python/lists.py
+++ Benchmarks/Python/lists.py
@@ -2,29 +2,34 @@ import timeit
 import random
 
 # We'll run the language's default sorting algorithm and a linear search algorithm on lists of different sizes
-# We'll run each test 1000 times
+# We'll run each test 5 times, we'll run each algorithm 1000 times per test
 
-# We'll print out the data in the following format, separated by newlines:
-# {LIST SIZE} - {TEST NUMBER} - {TEST TYPE} - {TIME TAKEN}
-
-# The test number will be 0-indexed, and will reset for each size
-
-# The test type will be either 'SORT' or 'SEARCH'
+# We'll use timeit to output our results
 
 # The list sizes we'll be using are 1 million items, 100,000 items, 10,000 items and 1,000 items
 # The list items will be randomly shuffled for each trial. This time will not be taken into account
 
-for size in (1000000, 100000, 10000, 1000):
-    for test in range(1000000):
-        dataset = list(range(size))
-        random.shuffle(dataset)
+def create_dataset(length):
+    dataset = list(range(length))
+    random.shuffle(dataset)
+    return dataset
+    
+def search(dataset):
+    for index, item in enumerate(dataset):
+        if item == 0:
+            return index
 
-        search_start = timeit.default_timer()
-        for item in dataset:
-            if item == 0:
-                print(f"{size} - {test} - SEARCH - {timeit.default_timer() - search_start}")
-                break
+def sort(dataset):
+    return sorted(dataset)
 
-        sort_start = timeit.default_timer()
-        dataset.sort()
-        print(f"{size} - {test} - SORT - {timeit.default_timer() - sort_start}")
+for operation in ("sort", "search"):
+    for size in (1000000, 100000, 10000, 1000):
+        print(f"Time to {operation} {size} items")
+        result = timeit.repeat(
+            setup = f"dataset = create_dataset({size})",
+            stmt = f"{operation}(dataset)",
+            times = 1000,
+            repeat = 5,
+        )
+        print(result)
+
