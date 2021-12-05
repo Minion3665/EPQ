ca149a6af32b286786308f79050c5b9f8a9dc9d6
Skyler Turner <skyler3665@gmail.com>
2021-12-05 23:18:05 +0000

    Update most recent note to add bibliography

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ((nil . (
-	 (org-roam-directory . "~/School/EPQ/Notes")
-	 (org-roam-db-location . "~/School/EPQ/Notes/org-roam.db")
+	 (org-roam-directory . "~/EPQ/Notes")
+	 (org-roam-db-location . "~/EPQ/Notes/org-roam.db")
 	 (eval git-auto-commit-mode 1)
 	 (org-roam-dailies-capture-templates ("n" "default" entry "* %?" :if-new
 					      (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n")
@@ -8,6 +8,6 @@
 					     ("d" "diary" entry "* Diary entry%?\n** What have I done since the last entry?\n** What have I learned & how?\n** What worked well?\n** What would I do differently next time?\n" :if-new
 					      (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n* Diary entry\n** What have I done since the last entry?\n** What have I learned & how?\n** What worked well?\n** What would I do differently next time?"))
 					     )
-	 (org-cite-global-bibliography "~/School/EPQ/Documents/bibliography.bib")
+	 (org-cite-global-bibliography "~/EPQ/Documents/bibliography.bib")
 	 (org-bibtex-export-arbitrary-fields 't)
 	 (org-bibtex-prefix . ""))))
